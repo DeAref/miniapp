96d8fbd39656cd142e01a8a5bd6c0038c64a2e76
Aref Solaimany <95649368+DeAref@users.noreply.github.com>
2025-02-16 22:57:02 +0330

    commit Validate.php

---
+++ validate.php
@@ -0,0 +1,47 @@
+<?php
+
+
+class Validate
+{
+    /**
+     * validate initData to ensure that it is from Telegram.
+     *
+     * @param string $botToken your bot token
+     * @param string $initData init data from Telegram (`Telegram.WebApp.initData`)
+     *
+     * @return bool return true if its from Telegram otherwise false
+     */
+    public static function isSafe(string $botToken, string $initData): bool
+    {
+        [$checksum, $sortedInitData] = self::convertInitData($initData);
+        $secretKey                   = hash_hmac('sha256', $botToken, 'WebAppData', true);
+        $hash                        = bin2hex(hash_hmac('sha256', $sortedInitData, $secretKey, true));
+
+        return 0 === strcmp($hash, $checksum);
+    }
+
+    /**
+     * convert init data to `key=value` and sort it `alphabetically`.
+     *
+     * @param string $initData init data from Telegram (`Telegram.WebApp.initData`)
+     *
+     * @return string[] return hash and sorted init data
+     */
+    private static function convertInitData(string $initData): array
+    {
+        $initDataArray = explode('&', rawurldecode($initData));
+        $needle        = 'hash=';
+        $hash          = '';
+
+        foreach ($initDataArray as &$data) {
+            if (substr($data, 0, \strlen($needle)) === $needle) {
+                $hash = substr_replace($data, '', 0, \strlen($needle));
+                $data = null;
+            }
+        }
+        $initDataArray = array_filter($initDataArray);
+        sort($initDataArray);
+
+        return [$hash, implode("\n", $initDataArray)];
+    }
+}
